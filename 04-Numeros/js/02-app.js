463c4eb0cb4607f22988808cadd8c310f74cdc1c
Carlos Cuellar <cc600142@gmail.com>
2024-02-06 08:56:46 -0600

    Numeros y operadores

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,26 @@
+const numero1 = 30;
+const numero2 = 20;
+
+
+let resultado;
+
+//Suma
+
+resultado = numer1 + numero2;
+
+// Resta
+
+resultado = numer1 - numero2;
+
+// Multiplicar
+
+resultado = numer1 * numero2;
+
+// Dividir
+
+resultado = numer1 / numero2;
+
+// Modulo
+resultado = numer1 % numero2;
+
+console.log(resultado);
---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,16 @@
+
+
+let puntaje = 5;
+puntaje ++; //5
+++puntaje;  //6
+
+let puntaje2 = 5;
+puntaje2--;  //5
+--puntaje2;  //4
+
+let puntaje3 = 5;
+puntaje3 += 3;
+puntaje -= 3;
+
+
+console.log(puntaje);
