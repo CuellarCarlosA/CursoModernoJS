0945f11e12b2c8dd5e277b18eeca6f8e3d5e0ac5
Carlos Cuellar <cc600142@gmail.com>
2024-02-06 14:59:20 -0600

    Booleans

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,12 @@
+const autenticado = true;
+
+if(autenticado){
+    console.log("Si puedes ver Netflix")
+} else {
+    console.log("No, no puedes ver netflix")
+}
+
+
+// Operador ternario
+
+console.log( autenticado ? 'Si esta autenticado' : 'no esta autenticado');
