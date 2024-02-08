455f182565b1f6f4552c3b09c9cfa0484079a946
Carlos Cuellar <cc600142@gmail.com>
2024-02-08 09:00:42 -0600

    Arrays 1/2

---
+++ 08-Arrays/js/04-app.js
@@ -0,0 +1,7 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+
+meses[0] = "nuevo mes";
+meses[7]="Ultimo mes";
+
+
+console.table(meses);
---
+++ 08-Arrays/js/05-app.js
@@ -0,0 +1,36 @@
+const meses = ['Enero', 'Febrero', 'Marzo'];
+
+
+//Agregar al final del arreglo
+meses.push("Abril");
+meses.push("Mayo");
+
+
+console.table(meses);
+
+
+const carrito = [];
+
+//Definir un producto 
+const producto = {
+    nombre: "Monitor de 24 pulgadas",
+    precio: 400,
+}
+const producto2 = {
+    nombre: "Celular",
+    precio: 800,
+}
+
+
+carrito.push(producto);
+carrito.push(producto2);
+
+const producto3 = {
+    nombre: "Teclado",
+    precio: 50,
+}
+
+carrito.unshift(producto3);
+
+
+console.log(carrito);
