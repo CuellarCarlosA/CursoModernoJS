d134361e469c74c90d3888fa577930f47abb7bd7
Carlos Cuellar <cc600142@gmail.com>
2024-02-20 08:39:33 -0600

    Arrow Functions

---
+++ 09-Funciones/js/12-app.js
@@ -0,0 +1,21 @@
+const carrito = [
+    { nombre: 'Monitor de 20 pulgadas', precio:500 },
+    { nombre: 'Televisión', precio:100 },
+    { nombre: 'Tablet', precio:200 },
+    { nombre: 'Audifonos', precio:300 },
+    { nombre: 'Teclado', precio:400 },
+    { nombre: 'Celular', precio:700 },
+]
+
+
+carrito.forEach( function(producto) {
+    console.log(`${producto.nombre} - Precio: ${producto.precio}` );
+} )
+
+const nuevoArreglo = carrito.map( (producto) =>{
+    return `${producto.nombre} - Precio: ${producto.precio}`;
+})
+
+const nuevoArreglo2 = carrito.forEach( (producto)=>{
+    return `${producto.nombre} - Precio: ${producto.precio}`;
+})
