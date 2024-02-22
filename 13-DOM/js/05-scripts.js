fce58bea29c105a8f4eac843175cc6013041bb10
Carlos Cuellar <cc600142@gmail.com>
2024-02-22 14:56:59 -0600

    Manejo del DOM 1

---
+++ 13-DOM/js/05-scripts.js
@@ -0,0 +1,11 @@
+const card = document.querySelectorAll('card');
+console.log(card);
+
+const formularios = document.querySelectorAll('#formulario');
+console.log(formularios);
+
+
+// si un elemento no existe 
+
+const noExiste = document.querySelectorAll('no-existe');
+console.log(noExiste);
