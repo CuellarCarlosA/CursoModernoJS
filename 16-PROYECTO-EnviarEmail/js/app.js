203b7a43034d095f5d4b092ad8f8b8117393d803
Carlos Cuellar <cc600142@gmail.com>
2024-06-20 22:15:03 -0600

    Proyecto Email P1

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    //Seleccionar los elementos de la interfas
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+
+//    console.log(inputMensaje);
+
+    //Asignar eventos
+    inputEmail.addEventListener('blur', validar);
+    inputAsunto.addEventListener('blur', validar);
+    inputMensaje.addEventListener('blur', validar);
+  
+    function validar(e) {
+        console.log(e.target.parentElement);
+
+        if(e.target.value.trim() === ''){
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+        }else{
+            console.log('si hay algo');
+        }
+    };
+
+    function mostrarAlerta(mensaje, referencia){
+    //Generar alerta en HTML
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+        //Inyectar el error al formulario
+        referencia.appendChild(error);
+    }
+
+});
